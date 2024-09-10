b7c67bfa20a0ec9f46ad94d05b895aa5f870675a
Mohamed Ahmed Mohamed Lemine <mohamed-lemine.mohamed-ahmed@grenoble-inp.org>
2024-09-10 12:45:20 +0200

    price

--- Equipe_2/src/hedge.cpp
+++ Equipe_2/src/hedge.cpp
@@ -1,7 +1,26 @@
 #include <iostream>
+#include "monte_carlo/monte_carlo.hpp"
+#include "Utils/convert.hpp"
+#include "monte_carlo/monte_carlo.hpp"
 
 int main(int argc, char *argv[])
 {
+    if(argc != 3) {
+        std::cout << "" << std::endl ; 
+    }
+
+    MonteCarlo* monte_carlo = convert_json_to_monte_carlo(argv[1] , argv[2]);
+
+    double prix = monte_carlo->price(0);
+    double prix_std_dev ; 
+    double erreur_couverture;
+
+
+    HedgingResults res(prix, prix_std_dev, erreur_couverture);
+    std::cout << res << std::endl;
+
+
+    delete monte_carlo;
 
     return 0;
 }
--- Equipe_2/src/price.cpp
+++ Equipe_2/src/price.cpp
@@ -1,8 +1,28 @@
 #include <iostream>
 #include "monte_carlo/monte_carlo.hpp"
+#include "Utils/convert.hpp"
+#include "monte_carlo/monte_carlo.hpp"
+#include "pnl/pnl_vector.h"
 
 int main(int argc, char *argv[])
 {
+    if(argc != 2) {
+        std::cout << "" << std::endl ; 
+    }
+
+    MonteCarlo* monte_carlo = convert_json_to_monte_carlo(argv[1]);
+
+    double prix = monte_carlo->price(0);
+    double prix_std_dev ; 
+    PnlVect* delta ;
+    PnlVect* delta_std_dev ;
+
+
+
+    PricingResults res(prix, prix_std_dev, delta, delta_std_dev);
+    std::cout << res << std::endl;
+
+    delete monte_carlo;
 
     return 0;
 }
